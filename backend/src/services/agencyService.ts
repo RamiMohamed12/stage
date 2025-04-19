223589a17b7a23f6192cb35fe8e148fd73946960
RamiMohamed12 <133769441+RamiMohamed12@users.noreply.github.com>
2025-04-19 17:05:37 +0200

    feat: implement getAllAgencies function to retrieve all agencies from the database

---
+++ backend/src/services/agencyService.ts
@@ -0,0 +1,27 @@
+import pool from '../config/db';
+import {Agency, AgencyRow} from '../models/Agency';
+import { PoolConnection, RowDataPacket } from 'mysql2/promise';
+import {ServiceErorr} from '../services/usersService';
+
+// function to get all the agencies from the database 
+// and return them as an array of Agency objects
+export const getAllAgencies = async (): Promise<Agency[]> => {
+    
+    let connection: PoolConnection | undefined; 
+    try{
+        connection = await pool.getConnection();
+        const sql = 'SELECT * FROM agency';
+        const [rows] = await connection.query<RowDataPacket[]>(sql);
+        if (rows.length === 0){
+            throw new ServiceErorr('No agencies found in the database.', 404);
+        }
+        return rows as Agency[]; 
+    } catch (error:any) {
+        console.error('Error retrieving agencies:', error.message);
+        throw new ServiceErorr('Error retrieving agencies.', 500);
+    } finally {
+        if (connection) {
+            connection.release(); 
+        }
+    }
+}
